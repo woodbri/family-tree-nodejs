3c35435575c6b6669fb85d9466271002390b93e8
Stephen Woodbridge <woodbri@swoodbridge.com>
2018-12-08 16:34:40 -0500

    Added ability to copy indi links from other photos.

--- routes/medialink.js
+++ routes/medialink.js
@@ -14,39 +14,46 @@ function mediaLinkRouter(req, res, next) {
 
     var dbName = req.params.dbName;
     var id = req.params.id;
-    var mode, indi;
+    var mode, indi, from_id;
 
     if (req.session && req.session.admin) {
+
+        var sql, params;
 
         if (req.query && req.query.del) {
             mode = 'del';
             indi = req.query.del;
+            sql = 'delete from indi_photos where indi=? and id=?';
+            params = [indi, id];
         }
         else if (req.query && req.query.add) {
             mode = 'add';
             indi = req.query.add;
+            sql = 'insert or ignore into indi_photos (indi, id) values(?,?)';
+            params = [indi, id];
+        }
+        else if (req.query && (req.query.cpy || req.query.cpy === '')) {
+            if (isNaN(parseInt(req.query.cpy))) {
+                console.log('cpy redirect');
+                res.redirect('/' + dbName + '/media/edit/' + id);
+                return;
+            }
+            mode = 'cpy';
+            from_id = req.query.cpy;
+            sql = 'insert or ignore into indi_photos select indi, ? as id from indi_photos where id=?';
+            params = [id, from_id];
         }
         else {
             next(createError('Invalid operation!'));
             return;
         }
 
-        if (isNaN(parseInt(id)) || ! indi.match(/^I\d+$/)) {
+        if (isNaN(parseInt(id)) || (mode != 'cpy' && ! indi.match(/^I\d+$/))) {
             next(createError("parameter(s) are not valid!"));
             return;
         }
 
         var pool = require('../db-config').createConnection(dbName);
-        var sql;
-        var params = [indi, id];
-
-        if (mode == 'add') {
-            sql = 'insert or ignore into indi_photos (indi, id) values(?,?)';
-        }
-        else if (mode == 'del') {
-            sql = 'delete from indi_photos where indi=? and id=?';
-        }
-
         pool.acquire(function(err, conn) {
             if (err) {
                 console.error(err);
