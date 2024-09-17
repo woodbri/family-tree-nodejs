fbc3b3961a374ba971e8ea3dbac49000e986ce96
Stephen Woodbridge <stephenwoodbridge37@gmail.com>
2024-09-17 18:13:36 -0400

    Refactored using ChatGPT.

--- bin/dump-db-as-ged.js
+++ bin/dump-db-as-ged.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-'use strict'
+'use strict';
 
-const fs = require('node:fs');
-const anyDB = require('any-db');
-const { parseArgs } = require('node:util');
-
+import { finished } from 'stream/promises';
+import fs from 'node:fs';
+import anyDB from 'any-db';
+import { parseArgs } from 'node:util';
 
 
 /**
@@ -68,256 +68,219 @@ function mapTag(name) {
 }
 
 function getDate() {
-  let date_time = new Date();
-  let date = ('0' + date_time.getDate()).slice(-2);
-  let month = ('0' + (date_time.getMonth() + 1)).slice(-2);
-  let year = date_time.getFullYear();
-  return year + '-' + month + '-' + date;
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Ensure 2 digits
+  const day = String(now.getDate()).padStart(2, '0');        // Ensure 2 digits
+  return `${year}-${month}-${day}`;
 }
 
-function getEvents(refid, conn, res) {
-  return new Promise((resolve, reject) => {
+async function getEvents(refid, conn, res) {
+  try {
     console.log('getEvents: ' + refid);
-    let sql = `select * from events where refid='${refid}' order by seq`;
+    const sql = `SELECT * FROM events WHERE refid = ? ORDER BY seq`;
     let results = '';
-    conn.query(sql)
-      .on('data', function (row) {
-        // console.log(row);
-        let tag = mapTag(row.ETYPE);
-        let result = '1 ' + tag + '\n';
-        if (row.DATE) result += '2 DATE ' + row.DATE + '\n';
-        if (row.PLAC) result += '2 PLAC ' + row.PLAC + '\n';
-        if (row.NOTE) result += '2 NOTE @' + row.NOTE + '@\n';
-        results += result;
-      })
-      .on('end', () => {
-        console.log('  end getEvents: ' + refid);
-        resolve(res + results)
-      })
-      .on('error', (err) => console.error(err));
+
+    await new Promise((resolve, reject) => {
+      conn.query(sql, [refid])
+        .on('data', (row) => {
+          let tag = mapTag(row.ETYPE);
+          results += `1 ${tag}\n`;
+          if (row.DATE) results += `2 DATE ${row.DATE}\n`;
+          if (row.PLAC) results += `2 PLAC ${row.PLAC}\n`;
+          if (row.NOTE) results += `2 NOTE @${row.NOTE}@\n`;
+        })
+        .on('end', resolve)
+        .on('error', reject);
+    });
+
+    console.log('  end getEvents: ' + refid);
+    return res + results;
+  } catch (error) {
+    console.error(`Error fetching events for ${refid}:`, error);
+    throw error;  // rethrow to be handled by caller
+  }
+}
+
+async function getFams(refid, conn, res) {
+  try {
+    console.log(`getFams: ${refid}`);
+    const sql = 'SELECT * FROM fams WHERE indi = ? ORDER BY seq';
+    
+    const results = await new Promise((resolve, reject) => {
+      let famsResults = '';
+      conn.query(sql, [refid])
+        .on('data', (row) => {
+          famsResults += `1 FAMS @${row.FAMI}@\n`;
+        })
+        .on('end', () => resolve(famsResults))
+        .on('error', reject);
+    });
+
+    console.log(`  end getFams: ${refid}`);
+    return res + results;
+  } catch (error) {
+    console.error(`Error fetching families for ${refid}:`, error);
+    throw error;
+  }
+}
+
+async function getFamc(refid, conn, res) {
+  try {
+    console.log(`getFamc: ${refid}`);
+    const sql = 'SELECT * FROM child WHERE indi = ? ORDER BY seq';
+
+    const results = await new Promise((resolve, reject) => {
+      let famcResults = '';
+      conn.query(sql, [refid])
+        .on('data', (row) => {
+          famcResults += `1 FAMC @${row.FAMI}@\n`;
+        })
+        .on('end', () => resolve(famcResults))
+        .on('error', reject);
+    });
+
+    console.log(`  end getFamc: ${refid}`);
+    return res + results;
+  } catch (error) {
+    console.error(`Error fetching FAMC for ${refid}:`, error);
+    throw error;
+  }
+}
+
+async function getChild(refid, conn, res) {
+  try {
+    console.log(`getChild: ${refid}`);
+    const sql = 'SELECT * FROM child WHERE fami = ? ORDER BY seq';
+
+    const results = await new Promise((resolve, reject) => {
+      let childResults = '';
+      conn.query(sql, [refid])
+        .on('data', (row) => {
+          childResults += `1 CHIL @${row.INDI}@\n`;
+        })
+        .on('end', () => resolve(childResults))
+        .on('error', reject);
+    });
+
+    console.log(`  end getChild: ${refid}`);
+    return res + results;
+  } catch (error) {
+    console.error(`Error fetching children for ${refid}:`, error);
+    throw error;
+  }
+}
+
+async function getNote(refid, conn, res) {
+  try {
+    console.log(`getNote: ${refid}`);
+    const sql = 'SELECT * FROM notes WHERE note = ? ORDER BY seq';
+
+    const results = await new Promise((resolve, reject) => {
+      let noteResults = '';
+      conn.query(sql, [refid])
+        .on('data', (row) => {
+          if (row.TEXT && row.TEXT.length > 0) {
+            let words = row.TEXT.split(/\s+/);
+            let line = '1 CONC ';
+            words.forEach((word) => {
+              if (line.length + word.length + 1 < 85) {
+                line += word + ' ';
+              } else {
+                noteResults += `${line}\n`;
+                line = `1 CONC ${word} `;
+              }
+            });
+            noteResults += `${line}\n`;
+          } else {
+            noteResults += '1 CONT\n';
+          }
+        })
+        .on('end', () => resolve(noteResults))
+        .on('error', reject);
+    });
+
+    console.log(`  end getNote: ${refid}`);
+    return res + results;
+  } catch (error) {
+    console.error(`Error fetching notes for ${refid}:`, error);
+    throw error;
+  }
+}
+
+async function processQuery(conn, query, processRow) {
+  return new Promise((resolve, reject) => {
+    let result = '';
+    conn.query(query)
+      .on('data', row => result += processRow(row))
+      .on('end', () => resolve(result))
+      .on('error', reject);
   });
 }
 
-function getFams(refid, conn, res) {
-  return new Promise((resolve, reject) => {
-    console.log('getFams: ' + refid);
-    let sql = `select * from fams where indi='${refid}' order by seq`;
-    let result = '';
-    conn.query(sql)
-      .on('error', console.error)
-      .on('data', function (row) {
-        // console.log(row);
-        result += '1 FAMS @' + row.FAMI + '@\n';
-      })
-      .on('end', function() {
-        console.log('  end getFams: ' + refid);
-        resolve( res + result );
-      })
-      .on('error', (err) => console.error(err));
-  })
+async function outputHeader(conn, writer) {
+  console.log('outputHeader');
+  const date = getDate();
+  const hdr = `0 HEAD\n1 SOUR family-tree-nodejs\n2 VERS 0.2.0\n3 WWW github.com/woodbri/family-tree-nodejs\n1 DATE ${date}\n1 CHAR UTF-8\n`;
+  writer.write(hdr);
 }
 
-function getFamc(refid, conn, res) {
-  return new Promise((resolve, reject) => {
-    console.log('getFamc: ' + refid);
-    let sql = `select * from child where indi='${refid}' order by seq`;
-    let result = '';
-    conn.query(sql)
-      .on('error', console.error)
-      .on('data', function (row) {
-        // console.log(row);
-        result += '1 FAMC @' + row.FAMI + '@\n';
-      })
-      .on('end', function() {
-        console.log('  end getFamc: ' + refid);
-        resolve( res + result );
-      })
-      .on('error', (err) => console.error(err));
-  })
+async function outputSubm(conn, writer) {
+  console.log('outputSubm');
+  writer.write('0 @SUBM@ SUBM\n');
 }
 
-function getChild(refid, conn, res) {
-  return new Promise((resolve, reject) => {
-    console.log('getChild: ' + refid);
-    let sql = `select * from child where fami='${refid}' order by seq`;
-    let result = '';
-    conn.query(sql)
-      .on('error', console.error)
-      .on('data', function (row) {
-        // console.log (row);
-        result += '1 CHIL @' + row.INDI + '@\n';
-      })
-      .on('end', function() {
-        console.log('  end getChild: ' + refid);
-        resolve( res + result );
-      })
-      .on('error', (err) => console.error(err));
-  })
+async function outputIndi(conn, conn2, writer) {
+  const query = 'select * from indi order by indi limit 5';
+  await processQuery(conn, query, async (row) => {
+    console.log('Fetching data for ' + row.INDI);
+    let res = `0 @${row.INDI}@ INDI\n1 NAME ${row.FNAME} /${row.LNAME}/\n2 SURN ${row.LNAME}\n2 GIVN ${row.FNAME}\n1 SEX ${row.SEX}\n`;
+    if (row.REFN) res += `1 REFN ${row.REFN}\n`;
+    if (row.NOTE) res += `1 NOTE @${row.NOTE}@\n`;
+    res = await getEvents(row.INDI, conn2, res);
+    res = await getFams(row.INDI, conn2, res);
+    res = await getFamc(row.INDI, conn2, res);
+    writer.write(res);
+  });
 }
 
-function getNote(refid, conn, res) {
-  return new Promise((resolve, reject) => {
-    console.log('getNote: ' + refid);
-    let sql = `select * from notes where note='${refid}' order by seq`;
-    let result = '';
-    conn.query(sql)
-      .on('error', console.error)
-      .on('data', function (row) {
-        let line = '1 CONC ';
-        if (row.TEXT && row.TEXT.length > 0) {
-          let words = row.TEXT.split(/\s+/);
-          words.forEach((w) => {
-            if (line.length + w.length + 1 < 85) {
-              line += w + ' ';
-            }
-            else {
-              result += line + '\n';
-              line = '1 CONC ' + w + ' ';
-            }
-          });
-          result += line + '\n';
-        }
-        else {
-          result += '1 CONT\n';
-          line = '1 CONT ';
-        }
-      })
-      .on('end', function () {
-        console.log('  end getNote: ' + refid);
-        resolve( res + result );
-      })
-      .on('error', (err) => console.error(err));
-  })
+async function outputFam(conn, conn2, writer) {
+  const query = 'select * from fami order by fami limit 5';
+  await processQuery(conn, query, async (row) => {
+    let res = `0 @${row.FAMI}@ FAM\n`;
+    if (row.HUSB) res += `1 HUSB @${row.HUSB}@\n`;
+    if (row.WIFE) res += `1 WIFE @${row.WIFE}@\n`;
+    if (row.NOTE) res += `1 NOTE @${row.NOTE}@\n`;
+    res = await getEvents(row.FAMI, conn2, res);
+    res = await getChild(row.FAMI, conn2, res);
+    writer.write(res);
+  });
 }
 
-function outputHeader(conn, writer) {
-  return new Promise(async (resolve, reject) => {
-    console.log('outputHeader');
-    let date = getDate();
-    let hdr = '0 HEAD\n'
-      + '1 SOUR family-tree-nodejs\n'
-      + '2 VERS 0.2.0\n'
-      + '3 WWW github.com/woodbri/family-tree-nodejs\n'
-      + '1 DATE ' + date + '\n'
-      + '1 CHAR UTF-8\n';
-    writer.write(hdr);
-    resolve();
-  })
+async function outputNotes(conn, conn2, writer) {
+  const query = 'select distinct note from notes order by note limit 20';
+  await processQuery(conn, query, async (row) => {
+    let res = `0 @${row.NOTE}@ NOTE\n`;
+    res = await getNote(row.NOTE, conn2, res);
+    writer.write(res);
+  });
 }
 
-function outputSubm(conn, writer) {
-  return new Promise(async (resolve, reject) => {
-    console.log('outputSubm');
-    writer.write('0 @SUBM@ SUBM\n');
-    resolve();
-  })
-}
-
-function outputIndi(conn, conn2, writer) {
-  return new Promise((resolve, reject) => {
-    console.log('outputIndi');
-    // indi, lname, fname, title, lname_mp, famc, sex, refn, note, bdate, ddate, living
-    let sql = 'select * from indi order by indi limit 5';
-    conn.query(sql)
-      .on('data', async function (row) {
-        console.log('Fetching data for ' + row.INDI);
-        let res = '0 @' + row.INDI + '@ INDI\n'
-          + '1 NAME ' + row.FNAME + ' /' + row.LNAME + '/\n'
-          + '2 SURN ' + row.LNAME + '\n'
-          + '2 GIVN ' + row.FNAME + '\n'
-          + '1 SEX ' + row.SEX + '\n';
-        if (row.REFN) res += '1 REFN ' + row.REFN + '\n';
-        if (row.NOTE) res += '1 NOTE @' + row.NOTE + '@\n';
-        res = await getEvents(row.INDI, conn2, res);
-        res = await getFams(row.INDI, conn2, res);
-        res = await getFamc(row.INDI, conn2, res);
-        writer.write(res);
-      })
-      .on('end', () => {
-        console.log('  outputIndi done');
-        resolve()
-      })
-      .on('error', (err) => console.error(err));
-  })
-}
-
-function outputFam(conn, conn2, writer) {
-  return new Promise((resolve, reject) => {
-    console.log('outputFam');
-    let sql = 'select * from fami order by fami limit 5';
-    conn.query(sql)
-      .on('data', async function (row) {
-        // console.log(row);
-        let res = '0 @' + row.FAMI + '@ FAM\n';
-          if (row.HUSB) res += '1 HUSB @' + row.HUSB + '@\n';
-          if (row.WIFE) res += '1 WIFE @' + row.WIFE + '@\n';
-          if (row.NOTE) res += '1 NOTE @' + row.NOTE + '@\n';
-          res = await getEvents(row.FAMI, conn2, res);
-          res = await getChild(row.FAMI, conn2, res);
-          writer.write(res);
-      })
-      .on('end', () => {
-        console.log('  outputFam done');
-        resolve()
-      })
-      .on('error', (err) => console.error(err));
-  })
-}
-
-function outputNotes(conn, conn2, writer) {
-  return new Promise((resolve, reject) => {
-    console.log('outputNotes');
-    let sql = 'select distinct note from notes order by note limit 20';
-    let lines = '';
-    conn.query(sql)
-      .on('data', async function(row) {
-        // console.log(row);
-        let res = `0 @${row.NOTE}@ NOTE\n`;
-        res = await getNote(row.NOTE, conn2, res);
-        writer.write(res);
-      })
-      .on('end', () => {
-        console.log('  outputNotes done');
-        resolve()
-      })
-      .on('error', (err) => console.error(err));
-  })
-}
-
-function outputSour(conn, writer) {
-  return new Promise(async (resolve, reject) => {
-    console.log('outputSour');
-    // TODO
-    resolve();
-  })
-}
-
-function outputRepo(conn, writer) {
-  return new Promise(async (resolve, reject) => {
-    console.log('outputRepo');
-    // TODO
-    resolve();
-  })
-}
-
-function outputTrlr(conn, writer) {
-  return new Promise(async (resolve, reject) => {
-    console.log('outputTrlr');
+async function outputTrlr(conn, writer) {
+  try {
     writer.write('0 TRLR\n');
-    resolve();
-  })
+  } catch (error) {
+    console.error('Error writing TRLR:', error);
+  }
 }
-
 
 async function main() {
-  
   const options = {
     'help': { type: 'boolean', short: 'h' },
     'db':   { type: 'string' },
     'ged':  { type: 'string' }
   };
-  const args = ['-h', '--db', '--ged'];
-  const {values, tokens} = parseArgs({options, token: true});
+  const { values } = parseArgs({ options });
 
   const dbfile = values.db ?? 'test.db';
   const ged = values.ged ?? 'test-out.ged';
@@ -327,46 +290,30 @@ async function main() {
     process.exit();
   }
 
-  console.log('dbfile: ', dbfile);
-  console.log('ged: ', ged);
+  const writer = fs.createWriteStream(ged, { flags: 'w', encoding: 'utf8' });
+  writer.on('error', (error) => console.log(`Write error: ${error.message}`));
 
-  const writer = fs.createWriteStream( ged, { flags: 'w', encoding: 'utf8' } );
-  writer.on('error', (error) => {
-    console.log(`An error occured while writing to the file. Error: ${error.message}`);
-    reject();
-  });
-  writer.on('finish', () => {
-    console.log(`All records have been written to ${ged}.`);
-    writer.close();
-    resolve();
-  });
-  console.log('writer created');
+  const conn = anyDB.createConnection({ adapter: 'sqlite3', database: dbfile });
+  const conn2 = anyDB.createConnection({ adapter: 'sqlite3', database: dbfile });
+  const conn3 = anyDB.createConnection({ adapter: 'sqlite3', database: dbfile });
 
-  let conn = anyDB.createConnection({
-    adapter: 'sqlite3',
-    database: dbfile
-  });
-  let conn2 = anyDB.createConnection({
-    adapter: 'sqlite3',
-    database: dbfile
-  });
-  let conn3 = anyDB.createConnection({
-    adapter: 'sqlite3',
-    database: dbfile
-  });
-  console.log('database conn and conn2 created');
+  try {
+    await outputHeader(conn, writer);
+    await outputSubm(conn, writer);
+    await outputIndi(conn, conn2, writer);
+    await outputFam(conn, conn2, writer);
+    await outputNotes(conn3, conn, writer);
+    await outputTrlr(conn, writer);
 
-  await outputHeader(conn, writer);
-  await outputSubm(conn, writer);
-  await outputIndi(conn, conn2, writer);
-  await outputFam(conn, conn2, writer);
-  await outputNotes(conn3, conn, writer);
-  await outputSour(conn, writer);
-  await outputRepo(conn, writer);
-  await outputTrlr(conn, writer);
-
+    await finished(writer); // Wait until writing is finished
+  } catch (err) {
+    console.error('Error occurred:', err);
+  } finally {
+    conn.end();
+    conn2.end();
+    conn3.end();
+  }
 }
 
-
 main();
- 
+
